c32d649176e729238e787d1c95ebcdd91ec3e0fa
Dmitriy <sharks22dm@gmail.com>
2024-12-04 01:37:43 +0300

    Добавление функционала о колебаниях акций и скриншоты использования кода

--- data_download.py
+++ data_download.py
@@ -25,3 +25,19 @@ def calculate_and_display_average_price(data):
     """
     average_price = data['Close'].mean()
     print(f'Средняя цена закрытия акций за период: {average_price:.2f}')
+
+
+def notify_if_strong_fluctuations(data, threshold):
+    """
+    Анализирует данные и уведомляет пользователя, если цена акций колебалась
+    более чем на заданный процент за период
+    """
+    max_price = data['Close'].max()
+    min_price = data['Close'].min()
+    fluctuation = (max_price - min_price) / min_price * 100
+    if fluctuation > threshold:
+        print(f'Цена акций колебалась более чем на {threshold:.2f}% за период. '
+              f'Колебание составило: {fluctuation:.2f}%')
+    else:
+        print(f'Цена акций колебалась менее чем на {threshold:.2f}% за период. '
+              f'Колебание составило: {fluctuation:.2f}%')
--- main.py
+++ main.py
@@ -9,11 +9,15 @@ def main():
     а затем передаёт результаты на визуализацию.
     """
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
-    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
-    print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: "
+          "AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), "
+          "AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+    print("Общие периоды времени для данных о запасах включают: "
+          "1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+    threshold = float(input("Введите порог колебания цены в процентах (например, '10'): "))
 
     # Fetch stock data
     stock_data = dd.fetch_stock_data(ticker, period)
@@ -26,6 +30,9 @@ def main():
 
     # Средняя цена закрытия акций за период
     dd.calculate_and_display_average_price(stock_data)
+
+    # Колебания цен за период
+    dd.notify_if_strong_fluctuations(stock_data, threshold)
 
 
 if __name__ == "__main__":
